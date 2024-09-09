25414f78899d4a3c721aef2471bee1d347e161e8
oliver grainge <oliver@grainge.me>
2024-09-09 14:11:09 +0100

    deleted binary layers

--- NeuroPress/QLayers/Binary/__init__.py
+++
@@ -1,2 +0,0 @@
-from .conv import Conv2dW1A1, Conv2dW1A16
-from .linear import LinearW1A1, LinearW1A16
--- NeuroPress/QLayers/__init__.py
+++ NeuroPress/QLayers/__init__.py
@@ -1,11 +1,5 @@
-from .Binary import Conv2dW1A1, Conv2dW1A16, LinearW1A1, LinearW1A16
+
 from .Integer import (
-    #Conv2dW2A8,
-    #Conv2dW2A16,
-    #Conv2dW4A8,
-    #Conv2dW4A16,
-    #Conv2dW8A8,
-    #onv2dW8A16,
     LinearW2A8,
     LinearW2A16,
     LinearW4A8,
@@ -13,6 +7,7 @@ from .Integer import (
     LinearW8A8,
     LinearW8A16,
 )
+
 from .Ternary import Conv2dWTA16, LinearWTA16
 
 ALL_QLINEARS = (LinearW8A16, LinearW4A16, LinearW8A8, LinearW4A8)
